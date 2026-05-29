624840ed510c521c84016eb3c902c5cc3a91b0a5
sato-yui-13 <k024g0002@g.neec.ac.jp>
2026-05-29 19:48:16 +0900

    それっぽいのあとは後で

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include "GameScene.h"
 
 using namespace KamataEngine;
+
+GameScene* gameScene = nullptr;
+
+
 //シーン切り替え
 enum class Scene {
 	kUnknown = 0,
@@ -14,89 +18,85 @@ enum class Scene {
 // 現在シーン（型）
 Scene scene = Scene::kUnknown;
 
+//ゲームシーンのインスタンス生成
 // 02_12 29枚目
-void ChangeScene() {
+//void ChangeScene() {
+//	auto input = Input::GetInstance();
+//
+//	
+//	switch (scene) {
+//
+//	case Scene::kTitle:
+//		if (input->PushKey(DIK_SPACE)) {
+//			scene = Scene::kGame;
+//
+//			if (!gameScene) {
+//				gameScene = new GameScene;
+//				gameScene->Initialize();
+//			}
+//		}
+//		break;
+//
+//	case Scene::kGame:
+//		if (input->PushKey(DIK_E)) {
+//			scene = Scene::kTitle;
+//
+//			delete gameScene;
+//			gameScene = nullptr;
+//		}
+//		break;
+//
+//	case Scene::kClear:
+//
+//	
+//
+//		if (Input::GetInstance()->PushKey(DIK_SPACE)) {
+//			scene = Scene::kTitle;
+//		}
+//
+//		break;
+//
+//		break;
+//
+//	case Scene::kOver:
+//		break;
+//	}
+//}
 
+void UpDataScene() {
+	//
 	switch (scene) {
-
 	case Scene::kTitle:
-		//if (titleScene->IsFinished()) {
-		//	// シーン変更
-		//	scene = Scene::kGame;
-		//	delete titleScene;
-
-
-
-		//	titleScene = nullptr;
-		//	gameScene = new GameScene;
-		//	gameScene->Initialize();
-		//}
+		//		titleScene->Update();
 		break;
-
 	case Scene::kGame:
-		// 02_12 30枚目
-		//if (gameScene->IsFinished()) {
-		//	// シーン変更
-		//	scene = Scene::kTitle;
-		//	delete gameScene;
-		//	gameScene = nullptr;
-		//	//titleScene = new TitleScene;
-		//	titleScene->Initialize();
-
-
-
-		//} else if (gameScene->IsFinished()) {
-		//	// HP0で死亡flag
-		//	scene = Scene::kOver;
-		//	//titleScene = new TitleScene;
-		//	titleScene->Initialize();
-		//	delete gameScene;
-		//	gameScene = nullptr;
-		//}
+		gameScene->Update();
 		break;
-
 	case Scene::kClear:
 		break;
-
 	case Scene::kOver:
 		break;
 	}
 }
+//
+// 02_12 32枚目
+void DrawScene() {
+	switch (scene) {
+	case Scene::kTitle:
+		//		titleScene->Draw();
+		break;
+	case Scene::kGame:
+			gameScene->Draw();
+		break;
+	case Scene::kClear:
+		//
+		break;
+	case Scene::kOver:
+		//
+		break;
+	}
+}
 
-//void UpDataScene() {
-//
-//	switch (scene) {
-//	case Scene::kTitle:
-//		titleScene->Update();
-//		break;
-//	case Scene::kGame:
-//		gameScene->Update();
-//		break;
-//	case Scene::kClear:
-//		break;
-//	case Scene::kOver:
-//		break;
-//	}
-//}
-//
-//// 02_12 32枚目
-//void DrawScene() {
-//	switch (scene) {
-//	case Scene::kTitle:
-//		titleScene->Draw();
-//		break;
-//	case Scene::kGame:
-//		gameScene->Draw();
-//		break;
-//	case Scene::kClear:
-//
-//		break;
-//	case Scene::kOver:
-//
-//		break;
-//	}
-//}
-//
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
@@ -106,10 +106,9 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
 
-	//ゲームシーンのインスタンス生成
-	GameScene* gameScene = new GameScene();
+
 	//ゲームシーンの初期化
-	gameScene->Initialize();
+	//gameScene->Initialize();
 
 
 	// メインループ
@@ -120,15 +119,15 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 		{
 			break;
 		}
-
-		//ゲームシーンの更新
-		gameScene->Update();
+		//ChangeScene();   // ★これ足す
+		UpDataScene();   // ★これ足す
 
 		// 描画更新
 		dxCommon->PreDraw();
 
 		//ゲームシーンの描画
-		gameScene->Draw();
+		///gameScene->Draw();
+		DrawScene();     // ★これも足す
 
 		// 描画終了
 		dxCommon->PostDraw();
@@ -136,6 +135,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 
 	//ゲームシーンの解放
 	delete gameScene;
+	//delete titleScene;
 	//nullptrの代入
 	gameScene = nullptr;
 
